f4c0f7525c7c783db63aba619cb37a9a58cc6c54
Bryan <Bwong727@gmail.com>
2018-05-26 11:01:29 -0400

    Completed Section three video 32 creating a functional component.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi Im a react app</h1>
+          <h1>Hi Im a react app</h1>
+          <p>This is really working</p>
+          <Person />
       </div>
     );
+
+      // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
