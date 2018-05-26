44b93f25880446d3c870b7212c8feef2d917c2ec
Bryan <Bwong727@gmail.com>
2018-05-26 11:57:44 -0400

    Completed videos 36-42 of section 3, adding states and manipulating states.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,14 +4,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Dude', age: 33 },
+            {name: 'SomeGuy', age: 34 },
+            {name: 'ThisGuy', age: 35 }
+        ],
+        otherState: 'some other value'
+    };
+
+    switchNameHandler = () => {
+        // console.log('was clicked');
+        // DONT DO THIS this.state.persons[0].name = 'ThatGuy';
+        this.setState({
+            persons: [
+                {name: 'ThatDude', age: 37 },
+                {name: 'SomeGuy', age: 34 },
+                {name: 'ThisGuy', age: 35 }
+            ]
+        })
+    };
   render() {
     return (
       <div className="App">
           <h1>Hi Im a react app</h1>
           <p>This is really working</p>
-          <Person />
-          <Person />
-          <Person />
+          <button onClick={this.switchNameHandler}>Switch name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobbies: running</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
 
