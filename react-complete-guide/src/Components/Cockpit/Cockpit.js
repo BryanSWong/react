abcf1d50fc833b48cca4d9ef5d15acd36a70e039
Bryan <Bwong727@gmail.com>
2018-07-23 18:50:34 -0400

    Completed videos 13-15 of section 7.

--- react-complete-guide/src/Components/Cockpit/Cockpit.js
+++ react-complete-guide/src/Components/Cockpit/Cockpit.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import classes from './Cockpit.css';
 
+import Aux from '../../hoc/Aux';
+
 const cockpit = (props) => {
 
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if(props.showPersons){
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
 
     if(props.persons.length <= 2){
@@ -19,11 +21,11 @@ const cockpit = (props) => {
     }
 
     return (
-      <div className={classes.Cockpit}>
+      <Aux>
           <h1>{props.appTitle}</h1>
           <p className={assignedClasses.join(' ')}>This is really working</p>
           <button className={btnClass} onClick={props.clicked}>Toggle Persons</button>
-      </div>
+      </Aux>
     );
 };
 
