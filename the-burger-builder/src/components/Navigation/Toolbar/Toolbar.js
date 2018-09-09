ada3cc75c9580db18392af78eb1b9ee0119ed5ce
Bryan <Bwong727@gmail.com>
2018-09-08 20:36:49 -0400

    Completed section 8

--- the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import Logo from '../../Logo/Logo'
+import Logo from '../../Logo/Logo';
 
 import classes from './Toolbar.css';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo/>
-        <nav>
-            ...
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
         </nav>
     </header>
 );
