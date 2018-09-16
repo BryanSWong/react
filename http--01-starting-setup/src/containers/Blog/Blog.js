1fff792af1eef79950e67366a27c206033b9b8a2
Bryan <Bwong727@gmail.com>
2018-09-16 00:11:21 -0400

    Code updates and changes

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -9,7 +9,9 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-      posts: []
+        posts: [],
+        selectedPostId: null,
+        error: false
     };
 
     componentDidMount () {
@@ -24,14 +26,30 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts});
                 // console.log(response);
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true});
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    };
+
     render () {
 
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>;
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if(!this.setState.error) {
+
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}/>;
+            });
+        }
 
         return (
             <div>
@@ -39,7 +57,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
