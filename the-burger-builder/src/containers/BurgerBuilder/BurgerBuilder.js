a1a3bea87ecbe55e0d50092bb1bca19cb2a1ef5c
Bryan <Bwong727@gmail.com>
2018-10-07 16:11:14 -0400

    code updates to section 10 error handler

--- the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
@@ -146,4 +147,4 @@ class BurgerBuilder extends Component{
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
