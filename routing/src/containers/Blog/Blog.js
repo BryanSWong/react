87fee655b8fd3edaa5c3aa06823c85bea0a67a9d
Bryan <Bwong727@gmail.com>
2018-10-22 16:43:29 -0400

    Completed section 11 video 6 preparing the project for routing.

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -3,52 +3,16 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 
 import './Blog.css';
+import Posts from  './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount () {
-        axios.get( '/posts' )
-            .then( response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                // console.log( response );
-            } )
-            .catch(error => {
-                // console.log(error);
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
 
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
+
 
         return (
             <div className="Blog">
-                <header className="Blog">
+                <header>
                     <nav>
                         <ul>
                             <li><a href="/">Home</a></li>
@@ -56,9 +20,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Blog">
-                    {posts}
-                </section>
+                <Posts />
             </div>
         );
     }
