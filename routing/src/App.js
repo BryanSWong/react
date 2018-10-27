96c2eb7401f82e21d901e31322344092906dbf22
Bryan <Bwong727@gmail.com>
2018-10-27 02:27:17 -0400

    Completed routing and server deployment.

--- routing/src/App.js
+++ routing/src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+        // <BrowserRouter basename="/">
         <BrowserRouter>
           <div className="App">
             <Blog />
