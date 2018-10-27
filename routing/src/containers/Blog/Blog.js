5a3bfacdb9e392ba0ae7f65a85a8a431a7f3a560
Bryan <Bwong727@gmail.com>
2018-10-26 23:31:51 -0400

    Completed redirecting routes.

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from  './Posts/Posts';
@@ -36,6 +36,7 @@ class Blog extends Component {
                 <Switch>
                     <Route path='/new-post' exact component={NewPost}/>
                     <Route path='/posts' component={Posts}/>
+                    <Redirect from="/" to="/posts"/>
                 </Switch>
 
             </div>
