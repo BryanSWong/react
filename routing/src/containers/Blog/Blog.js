34efe2d1f819c2aa0a78373de3d11e7d1d96f5b8
Bryan <Bwong727@gmail.com>
2018-10-27 00:00:39 -0400

    Completed handleing the 404 route cases.

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -38,7 +38,9 @@ class Blog extends Component {
                 <Switch>
                     { this.state.auth ? <Route path='/new-post' exact component={NewPost}/> : null}
                     <Route path='/posts' component={Posts}/>
-                    <Redirect from="/" to="/posts"/>
+                    <Route render={() => <h1>Not found</h1>}/>
+                    {/* <Redirect from="/" to="/posts"/> */}
+                    {/* <Route path='/posts' component={Posts}/> */}
                 </Switch>
 
             </div>
