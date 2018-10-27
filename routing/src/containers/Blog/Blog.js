a3acf574bd6f8222dca00041baf1d819ad9e2d51
Bryan <Bwong727@gmail.com>
2018-10-27 00:47:11 -0400

    Completed lazy routing.

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -4,12 +4,17 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from  './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     };
 
     render () {
@@ -36,7 +41,7 @@ class Blog extends Component {
                 {/*<Route path="/"  exact render={() => <h1>hi</h1> }/> */}
 
                 <Switch>
-                    { this.state.auth ? <Route path='/new-post' exact component={NewPost}/> : null}
+                    { this.state.auth ? <Route path='/new-post' exact component={AsyncNewPost}/> : null}
                     <Route path='/posts' component={Posts}/>
                     <Route render={() => <h1>Not found</h1>}/>
                     {/* <Redirect from="/" to="/posts"/> */}
